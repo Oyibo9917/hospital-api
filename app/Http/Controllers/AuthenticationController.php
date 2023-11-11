69e7db35e2acecb5800b6b138596f39fc1f69079
Peace <peace9917@gmail.com>
2023-11-11 01:24:26 +0100

    Initial commit

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Hash;
+
+class AuthenticationController extends Controller
+{
+    public function register(Request $request){
+        $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|unique:users|max:255',
+            'password' => 'required|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('auth_token')->accessToken;
+
+        return response([
+            'token' => $token
+        ]);
+    }
+
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)) {
+            return response([
+                'message' => 'The provider credencials are incorrect'
+            ], 404);
+        }
+
+        $token = $user->createToken('auth_token')->accessToken;
+
+        return response([
+            'token' => $token
+        ], 200);
+
+    }
+
+    public function logout(Request $request){
+        $request->user()->token()->revoke();
+
+        return response([
+            'message' => 'logged out successfully'
+        ], 200);
+    }
+}
